fa0b11a7bee31ae3272ea0353aba530b13d3f3ed
Eryk <emroczko@me.com>
2023-01-14 02:29:27 +0100

    Added working main

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use crate::nbc::nbc;
-use ndarray::array;
+use crate::vector_reader::read_vectors_from_file;
 
 mod nbc;
 mod neighbourhood;
+mod vector_reader;
 
 fn main() {
     println!("NBC clustering");
-
-    let vectors = array!([1.0, 2.0, 3.0, 4.0]);
-
-    let k = 2;
-    let _ = nbc(vectors, k);
+    let dimension = 2;
+    let vectors = read_vectors_from_file("dataset.txt", dimension);
+    let nbc_res = nbc(vectors, 20);
+    println!("{:?}", nbc_res);
 }
+
+#[cfg(test)]
+mod tests {}
