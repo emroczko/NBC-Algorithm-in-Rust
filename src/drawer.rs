b377b4d7727bf45fcdeac92a8dbed018cc9752a2
Eryk <emroczko@me.com>
2023-01-16 02:33:58 +0100

    Create drawer.rs

---
+++ src/drawer.rs
@@ -0,0 +1,33 @@
+use plotters::prelude::*;
+
+pub fn draw_clustered_data(data: &Vec<(f64, f64, i32)>) {
+    let root_area = BitMapBackend::new("clustered.png", (600, 400)).into_drawing_area();
+    root_area.fill(&WHITE).unwrap();
+
+    let mut ctx = ChartBuilder::on(&root_area)
+        .set_label_area_size(LabelAreaPosition::Left, 40)
+        .set_label_area_size(LabelAreaPosition::Bottom, 40)
+        .caption("Clustering results", ("sans-serif", 40))
+        .build_cartesian_2d(-3.0..3.0, -1.5..2.0)
+        .unwrap();
+
+    ctx.configure_mesh().draw().unwrap();
+
+    ctx.draw_series(data.iter().map(|(coord_x, coord_y, group)| {
+        let color = if *group == -1 {
+            BLACK.mix(1.0)
+        } else {
+            Palette99::pick(*group as usize).mix(0.9)
+        };
+        Circle::new(
+            (*coord_x, *coord_y),
+            5,
+            ShapeStyle {
+                color,
+                filled: true,
+                stroke_width: 0.25 as u32,
+            },
+        )
+    }))
+    .unwrap();
+}
