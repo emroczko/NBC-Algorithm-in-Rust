b197eaa6cc8ae553290238091663f493cda1c473
Eryk <emroczko@me.com>
2023-01-16 02:34:13 +0100

    Added method to create data for drawing

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ use crate::nbc::nbc;
 use crate::neighbourhood::RowId;
 use crate::vector_reader::read_vectors_from_file;
 use ndarray::Array2;
-use std::collections::BTreeMap;
+use std::collections::btree_map::BTreeMap;
 
+mod drawer;
 mod nbc;
 mod neighbourhood;
 mod vector_reader;
@@ -13,8 +14,25 @@ fn main() {
     println!("NBC clustering");
     let dimension = 2;
     let vectors = read_vectors_from_file("dataset.txt", dimension);
-    let nbc_res = nbc(vectors, 20);
+    let nbc_res = nbc(&vectors, 20);
+    let merged_data = merge_data(&vectors, &nbc_res);
+    draw_clustered_data(&merged_data);
     println!("{:?}", nbc_res);
+}
+
+fn merge_data(
+    vectors: &Array2<f64>,
+    clustered_data: &BTreeMap<RowId, i32>,
+) -> Vec<(f64, f64, i32)> {
+    let mut result: Vec<(f64, f64, i32)> = Vec::new();
+    for (index, row) in vectors.rows().into_iter().enumerate() {
+        let group = clustered_data.get(&(index as i32)).expect("EE");
+        let coord_x = *row.get(0).expect("");
+        let coord_y = *row.get(1).expect("");
+        result.push((coord_x, coord_y, *group));
+    }
+
+    return result;
 }
 
 #[cfg(test)]
