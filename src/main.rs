3ac7d42bf1e8ce5796712322162700e8cfcbbf34
Eryk <emroczko@me.com>
2023-01-16 10:38:51 +0100

    Added using input arguments

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use crate::drawer::draw_clustered_data;
 use crate::nbc::nbc;
 use crate::neighbourhood::RowId;
-use crate::vector_reader::read_vectors_from_file;
+use crate::vector_manager::{read_vectors_from_file, write_to_file};
 use clap::Parser;
 use std::collections::btree_map::BTreeMap;
 use std::time::Instant;
@@ -9,35 +9,43 @@ use std::time::Instant;
 mod drawer;
 mod nbc;
 mod neighbourhood;
-mod vector_reader;
+mod vector_manager;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     #[arg(short, long)]
-    dataset_path: String,
+    file_with_dataset: String,
     #[arg(short, long)]
     dataset_dimension: usize,
-    #[arg(short, long, default_value_t = 1)]
-    k_value: u8,
+    #[arg(short, long, default_value_t = 10)]
+    k_value: i32,
     #[arg(short, long, default_value_t = true)]
     plot_data: bool,
 }
 
 fn main() {
-    let dimension = 2;
-    let file_name = "dataset.txt";
+    let args = Args::parse();
 
-    let vectors = read_vectors_from_file(file_name, dimension);
+    let dimension = args.dataset_dimension;
+    let file_name = args.file_with_dataset;
+    let k_value = args.k_value;
+
+    let vectors = read_vectors_from_file(&file_name, dimension);
 
     println!("Starting NBC...");
     let start = Instant::now();
-    let nbc_res = nbc(&vectors, 15);
+    let nbc_res = nbc(&vectors, k_value);
     let duration = start.elapsed();
     println!("NBC algorithm for file {} took: {:?}", file_name, duration);
 
     let merged_data = merge_data(&vectors, &nbc_res);
-    draw_clustered_data(&merged_data, file_name);
+
+    write_to_file(&merged_data);
+
+    if args.plot_data && dimension == 2 {
+        draw_clustered_data(&merged_data, &file_name);
+    }
 }
 
 fn merge_data(
