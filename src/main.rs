c99c1a8b593dc8369ab14e133094245e49f99884
Eryk <emroczko@me.com>
2023-01-16 03:22:12 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use crate::drawer::draw_clustered_data;
 use crate::nbc::nbc;
 use crate::neighbourhood::RowId;
 use crate::vector_reader::read_vectors_from_file;
+use clap::Parser;
 use ndarray::Array2;
 use std::collections::btree_map::BTreeMap;
 use std::time::Instant;
@@ -11,14 +12,28 @@ mod nbc;
 mod neighbourhood;
 mod vector_reader;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    dataset_path: String,
+    #[arg(short, long)]
+    dataset_dimension: usize,
+    #[arg(short, long, default_value_t = 1)]
+    k_value: u8,
+    #[arg(short, long, default_value_t = true)]
+    plot_data: bool,
+}
+
 fn main() {
     let dimension = 2;
-    let file_name = "dataset.txt";
+    let file_name = "datasets/engytime";
 
     let vectors = read_vectors_from_file(file_name, dimension);
 
+    println!("Starting NBC...");
     let start = Instant::now();
-    let nbc_res = nbc(&vectors, 20);
+    let nbc_res = nbc(&vectors, 15);
     let duration = start.elapsed();
     println!("NBC algorithm for file {} took: {:?}", file_name, duration);
 
